7a07e7d011f9cbd8969f9d891028f3972e48b398
gabrielCampos1325 <camposgabriel372@gmail.com>
2024-04-22 08:23:34 -0300

    all five points done

--- Homework_3/index.js
+++ Homework_3/index.js
@@ -12,7 +12,7 @@ let products = [{name: "milk", price: 5},
                 {name: "yeast", price: 1}];
 let discount = 15; 
 console.log(JSON.stringify(calculateDiscountedPrice(products, discount)));
- */
+*/
 
 const calculateTotalPrice = function(products) {
     let total = 0;
@@ -21,21 +21,40 @@ const calculateTotalPrice = function(products) {
 }
 /* 
 console.log(calculateTotalPrice(products));
- */
+*/
 
 const getFullName = function(person) {
     return person.firstName + ' ' + person.lastName;
 }
 /* 
-console.log(getFullName({firstName: "Gabriel", lastName: "Camposs"}));
- */
+console.log(getFullName({firstName: "Gabriel", lastName: "Campos"})); 
+*/
 
 const separateWords = txt => txt.split(" ");
 const filterDuplicates = words => [...new Set(words)];
 const sortWords = words =>  [...words].sort();
 /* 
-console.log(sortWords(filterDuplicates(separateWords("Hello word Hello"))));
- */
+console.log(sortWords(filterDuplicates(separateWords("Hello word Hello")))); 
+*/
+
+const getAverageGrade = function(arr) {
+    let avg = 0;
+    arr.forEach(e => avg += e);
+    return avg / arr.length;
+}
+
+const studentGrades = function(arr) {
+    return arr.map(s => getAverageGrade(s.grades));
+} 
+
+const students = [
+    { firstName: 'Juan', grades: [6, 3, 4] },
+    { firstName: 'Pedro', grades: [6, 7, 9] },
+    { firstName: 'Alberto', grades: [2, 4, 10] },
+];
+/* 
+console.log(getAverageGrade(studentGrades(students))); 
+*/
 
 const createCounter = () => {
     let count = 0;
@@ -44,9 +63,10 @@ const createCounter = () => {
     
 const firstCount = createCounter();
 const secondCount = createCounter();
-/* console.log(firstCount());
-console.log(secondCount()); */
-
+/* 
+console.log(firstCount());
+console.log(secondCount()); 
+*/
 
 const repeatFunction = function(f, n) {
     if (n >= 0) {
@@ -63,10 +83,10 @@ const repeatFunction = function(f, n) {
         }
     }
 }
-
-/* repeatFunction(() => {console.log("Gabriel Campos")}, 4)();
-repeatFunction(() => {console.log("Gabriel Campos")}, -1)(); */
-
+/* 
+repeatFunction(() => {console.log("Gabriel Campos")}, 4)();
+repeatFunction(() => {console.log("Gabriel Campos")}, -1)(); 
+*/
 
 function calculateFactorial(n) {
     if (n > 0) {
@@ -76,8 +96,8 @@ function calculateFactorial(n) {
     }
 }
 /* 
-console.log(calculateFactorial(5)); */
-
+console.log(calculateFactorial(5)); 
+*/
 
 function power(base, exponent) {
     if (exponent === 0) return 1;
@@ -89,8 +109,41 @@ function power(base, exponent) {
     }
 }
 /* 
-console.log(power(2, 3)); */
+console.log(power(2, 3)); 
+*/
 
+const lazyMap = function(arr, mapFunction) {
+    let i = -1;
+    return function() {
+        if (i < arr.length) {
+            i++;
+            return mapFunction(arr[i]);            
+        }        
+    }
+}
 
+/* 
+const arr = [10, 12, 5, 3, 8];
+const getValue = lazyMap(arr, x => x + 3);
+console.log(getValue());
+console.log(getValue());
+console.log(getValue());  
+*/
 
+const fibonacciGenerator = function() {
+    let x = 0;
+    let y = 1;
+    return function() {
+        let answer = x + y;
+        x = y;
+        y = answer;
+        return answer;
+    };
+}
 
+/* 
+const getNextNumber = fibonacciGenerator();
+console.log(getNextNumber());
+console.log(getNextNumber());
+console.log(getNextNumber()); 
+*/
